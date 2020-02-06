964dcd13c567d780143013acb078acfc9d13ff58
Nicolas Guimier <nicolas.g@flexishore.com>
2020-02-06 13:11:46 +0100

    add meta image

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,10 +7,10 @@ module MetaTagsHelper
     content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
-  #def meta_image
-  #  meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # ajoutez la ligne ci-dessous pour que le helper fonctionne indifféremment
     # avec une image dans vos assets ou une url absolue
-  #  meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
-  #end
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
 end
