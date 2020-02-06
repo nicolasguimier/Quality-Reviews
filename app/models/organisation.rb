2221dee582ac79c61bf67fd36f042b7e4243874d
Nicolas Guimier <nicolas.g@flexishore.com>
2020-02-06 01:05:13 +0100

    try to define friendly urls

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,17 @@
 class Organisation < ApplicationRecord
   belongs_to :user
+  validates_uniqueness_of :name
+
+  # this will generate friendly urls
+  # def to_param
+  #   name.parameterize
+  # end
+
+  # to allow using "Organisation.find(params[:id])" in the contrleur,
+  # even with friendy urls
+  # def self.find(input)
+  #   find_by_name(input)
+  # end
+
+  # better use a gem : https://github.com/norman/friendly_id
 end
